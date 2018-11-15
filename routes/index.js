e17a4b0b338bba66e9d7491870e695ddc9d30db1
Scott Hudson <shudson5523@gmail.com>
2018-11-15 08:11:21 -0500

    Added host page and began styling

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,8 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/host-family', function(req, res, next) {
+  res.render('host', { title: 'Express' });
+});
+
 module.exports = router;
